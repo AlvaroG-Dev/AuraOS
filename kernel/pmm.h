c5883e4343ea2cfe6879caec2795f9becc6fca7a
Alvaro G. <jk123yl2@gmail.com>
2026-08-08 11:40:45 +0200

    phase A: strengthen PMM interface

--- kernel/pmm.h
+++ kernel/pmm.h
@@ -1,12 +1,17 @@
 #pragma once
+
 #include <stdint.h>
-#include "paging.h"  // PAGE_SIZE
+#include "paging.h"
 
-// Macros auxiliares para bitmap
-#define BITMAP_SET(bitmap, bit)   ((bitmap)[(bit) / 8] |= (1 << ((bit) % 8)))
-#define BITMAP_CLEAR(bitmap, bit) ((bitmap)[(bit) / 8] &= ~(1 << ((bit) % 8)))
-#define BITMAP_TEST(bitmap, bit)  ((bitmap)[(bit) / 8] & (1 << ((bit) % 8)))
+#define BITMAP_SET(bitmap, bit)   ((bitmap)[(bit) / 8] |= (uint8_t)(1u << ((bit) % 8)))
+#define BITMAP_CLEAR(bitmap, bit) ((bitmap)[(bit) / 8] &= (uint8_t)~(1u << ((bit) % 8)))
+#define BITMAP_TEST(bitmap, bit)  ((bitmap)[(bit) / 8] & (uint8_t)(1u << ((bit) % 8)))
 
-void pmm_init(uint64_t memmap, uint64_t memmap_size, uint64_t memmap_desc_size);
+int pmm_init(uint64_t memmap, uint64_t memmap_size, uint64_t memmap_desc_size);
 uint64_t pmm_alloc_page(void);
 void pmm_free_page(uint64_t phys_addr);
+
+uint64_t pmm_total_pages(void);
+uint64_t pmm_used_pages(void);
+uint64_t pmm_free_pages(void);
+int pmm_is_ready(void);
